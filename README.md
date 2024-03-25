ef91efdfd9e62f286051da6e9d8f15c1da232995
samira-nasser <samiranasser520@gmail.com>
2024-03-25 02:18:39 +0200

    add swagger docs

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ School Management System application
 - Add/Edit/Delete/index Student
 - Add/Edit/Delete/index Classroom
 - Add/Edit/Delete/index School
+- Add/Edit/Delete/index UserType
 - Roles/Permissions as for student and Admin Role
 
 # Getting started
@@ -28,3 +29,21 @@ School Management System application
 - create your .env file as similar to .env.example
 - Write in terminal (nodemon) to run the project server
 - open browser with localhost:3000 as 3000 the default port
+
+## .ENV FILE
+
+.env file should conatin the following data:
+
+> BASE_URL=http://localhost:3000
+
+> PORT = 3000
+
+> DB_NAME = 'schooldb'
+
+> JWT_SECRET ='top_secret'
+
+> DB_NAME = "schooldb"
+
+> DB_PORT=27017
+
+> DB_HOST=
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const dotenv = require("dotenv");
 const indexRouter = require("./routes/index");
 const studentsRouter = require("./routes/studentRoutes");
@@ -24,6 +26,40 @@ app.use("/users", userRouter);
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server");
 });
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
